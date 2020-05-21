6b1aa6f25a079f2172f2ff36028d2e7636cc0721
jthaapas <jthaapas@jyu.fi>
2020-05-21 12:46:26 +0300

    Tutorial: Chapter 10 ~ Valmis.
    Välitetään dataa ja tapahtumia Vaadin komponenttien välillä.

--- src/main/java/com/marjakuusi/register/ui/MainView.java
+++ src/main/java/com/marjakuusi/register/ui/MainView.java
@@ -6,10 +6,12 @@ import com.marjakuusi.register.backend.service.CompanyService;
 import com.marjakuusi.register.backend.service.ContactService;
 
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -50,10 +52,15 @@ public class MainView extends VerticalLayout {
         setSizeFull();
 
         configureGrid();
-        configureFilter(); // Method call for what filter should do
+        // configureFilter(); // Method call for what filter should do
 
         // Initialize the form in the constructor.
         form = new ContactForm(companyService.findAll());
+
+        // Listeners to forms.
+        form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+        form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+        form.addListener(ContactForm.CloseEvent.class, e -> closeEditor());
 
         // Creates HTML Div, that wraps Grid and form.
         Div content = new Div(grid, form);
@@ -62,8 +69,33 @@ public class MainView extends VerticalLayout {
 
         // Adds elements to Visible UI
         // add(filterText, content);
-        add(filterText, content);
+        add(getToolbar(), content);
         updateList();
+
+        /* Clears old values, hides the form and removes "editing" CSS class */
+        closeEditor();
+    }
+
+    /**
+     * Method uses contactService to save the contact in the event to the data
+     * base. Then method updates the list and closes the editor.
+     * @param event event that is handled
+     */
+    private void saveContact(ContactForm.SaveEvent event) {
+        contactService.save(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    /**
+     * Method uses contactService to delete contact from the database, then
+     * method updates list and clsoes the editor.
+     * @param event event that is handled
+     */
+    private void deleteContact(ContactForm.DeleteEvent event) {
+        contactService.delete(event.getContact());
+        updateList();
+        closeEditor();
     }
 
     /**
@@ -93,6 +125,27 @@ public class MainView extends VerticalLayout {
          * This piece of code loops through every column.
          */
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        /*
+         * Adds listener to the grid, we only want Contact so we use
+         */
+        grid.asSingleSelect().addValueChangeListener(event ->
+                editContact(event.getValue()));
+    }
+
+    /**
+     * Sets selected contact in the ContactForm and hides or shows the form,
+     * depending on the selection. IT also sets the "editing" CSS class name
+     * when editing.
+     */
+    public void editContact(Contact contact) {
+        if (contact == null) {
+            closeEditor();
+        } else {
+            form.setContact(contact);
+            form.setVisible(true);
+            addClassName("editing");
+        }
     }
 
     /**
@@ -121,6 +174,44 @@ public class MainView extends VerticalLayout {
      */
     private void updateList() {
         grid.setItems(contactService.findAll(filterText.getValue()));
+    }
+
+    /**
+     * Call at the end of the constructor Sets,
+     * clearing out old values and hides the form.
+     */
+    private void closeEditor() {
+        form.setContact(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
+
+    /**
+     * Creates layout, that wraps textfield and button, renames configureFilter
+     * method to configureToolbar and replace the contents as follows.
+     * @return Returns the HorizontalLayout Component
+     */
+    private HorizontalLayout getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+
+        Button addContactButton = new Button("Add contact");
+        addContactButton.addClickListener(click -> addContact());
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
+
+    /**
+     * Deleselectes the grid and creates new Contact Object and passes that
+     * to editContact method.
+     */
+    void addContact() {
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
     }
 
 }
